ae7ecaef08a6ea2af38087283ccb82d06b6bb18b
Sara-Jo <sarajo.dev@gmail.com>
2024-04-27 14:09:27 +0900

    add: menu list for episode page

--- src/app/list/[episode]/page.tsx
+++ src/app/list/[episode]/page.tsx
@@ -1,3 +1,11 @@
+import styles from "./page.module.css";
+
 export default function Episode({ params }: { params: { episode: number } }) {
-  return <div>My Post: {params.episode}</div>;
+  return (
+    <div className={styles.main}>
+      <p className={styles.element}>📄 전체 스크립트 보기</p>
+      <p className={styles.element}>✍️ 주요 표현 공부하기</p>
+      <p className={styles.element}>💯 Quiz</p>
+    </div>
+  );
 }
