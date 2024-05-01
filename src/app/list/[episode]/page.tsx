823d3e154fee2e9dfcaf1be2f7f959a98e4d3a79
Sara-Jo <sarajo.dev@gmail.com>
2024-05-01 17:28:53 +0900

    feat: show episode title on episode pages

--- src/app/list/[episode]/page.tsx
+++ src/app/list/[episode]/page.tsx
@@ -2,12 +2,17 @@
 
 import { useRouter } from "next/navigation";
 import styles from "./page.module.css";
+import { episodeData } from "@/db/episodes";
 
 export default function Episode({ params }: { params: { episode: number } }) {
   const router = useRouter();
+  const episodeTitle = episodeData[params.episode - 1].title;
 
   return (
     <div className={styles.main}>
+      <p className={styles.episodeTitle}>
+        Episode {params.episode}: {episodeTitle}
+      </p>
       <p className={styles.element}>📄 전체 스크립트 보기</p>
       <p
         className={styles.element}
