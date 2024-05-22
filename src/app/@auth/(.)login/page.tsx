adf02f08e28a42119e0cdb5fa35a7d9fdfca6d20
Sara-Jo <sarajo.dev@gmail.com>
2024-05-22 11:51:04 +0900

    add: close modal button

--- src/app/@auth/(.)login/page.tsx
+++ src/app/@auth/(.)login/page.tsx
@@ -1,10 +1,26 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import styles from "./page.module.css";
 import Login from "@/app/_components/login";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 
 export default function Page() {
+  const router = useRouter();
+
   return (
     <div className={styles.overlay}>
       <div className={styles.container}>
+        <div className={styles.closeButtonWrapper}>
+          <div
+            onClick={() => {
+              router.back();
+            }}
+            className={styles.closeButton}
+          >
+            <CloseRoundedIcon />
+          </div>
+        </div>
         <Login />
       </div>
     </div>
