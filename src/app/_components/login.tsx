5bf3ab9293db61234a15e8c827215c8f8f55bdb6
Sara-Jo <sarajo.dev@gmail.com>
2024-05-22 10:32:28 +0900

    add: supabase ui

--- src/app/_components/login.tsx
+++ src/app/_components/login.tsx
@@ -1,3 +1,18 @@
+"use client";
+
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { createClient } from "@supabase/supabase-js";
+
 export default function Login() {
-  return <div>login</div>;
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+  );
+
+  return (
+    <div>
+      <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+    </div>
+  );
 }
