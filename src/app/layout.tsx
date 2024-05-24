914925a3597d63fb880482321f2216a573006c6e
Sara-Jo <sarajo.dev@gmail.com>
2024-05-24 17:52:38 +0900

    feat: check isLoggedIn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import SupabaseProvider from "./auth/supabaseProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,8 +23,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div>{auth}</div>
-        {children}
+        <SupabaseProvider>
+          <div>{auth}</div>
+          {children}
+        </SupabaseProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,43 @@
 import { useRouter } from "next/navigation";
 import styles from "./page.module.css";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
 
 export default function Home() {
   const router = useRouter();
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+  );
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const logout = async () => {
+    const { error } = await supabase.auth.signOut();
+    setIsLoggedIn(false);
+  };
+
+  useEffect(() => {
+    const getUser = async () => {
+      const user = await supabase.auth.getUser();
+      const session = await supabase.auth.getSession();
+      if (session.data.session !== null) setIsLoggedIn(true);
+    };
+    getUser();
+  }, [supabase]);
 
   return (
     <main className={styles.main}>
       <div className={styles.upperWrapper}>
-        <Link href="/login">
-          <p className={styles.loginButton}>Sign in</p>
-        </Link>
+        {isLoggedIn ? (
+          <p onClick={logout} className={styles.loginButton}>
+            Logout
+          </p>
+        ) : (
+          <Link href="/login">
+            <p className={styles.loginButton}>Sign in</p>
+          </Link>
+        )}
       </div>
       <div className={styles.body}>
         <h1>English With Suits</h1>
