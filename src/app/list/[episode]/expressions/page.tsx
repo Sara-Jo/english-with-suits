eec22c17d7ea755e088d747966d62186dda75d7f
Sara-Jo <sarajo.dev@gmail.com>
2024-05-28 16:09:32 +0900

    add: bookmark button

--- src/app/list/[episode]/expressions/page.tsx
+++ src/app/list/[episode]/expressions/page.tsx
@@ -5,6 +5,10 @@ import styles from "./page.module.css";
 import { useEffect, useState } from "react";
 import VolumeUpRoundedIcon from "@mui/icons-material/VolumeUpRounded";
 import LaunchRoundedIcon from "@mui/icons-material/LaunchRounded";
+import BookmarkBorderRoundedIcon from "@mui/icons-material/BookmarkBorderRounded";
+import BookmarkRoundedIcon from "@mui/icons-material/BookmarkRounded";
+import ArrowCircleLeftRoundedIcon from "@mui/icons-material/ArrowCircleLeftRounded";
+import ArrowCircleRightRoundedIcon from "@mui/icons-material/ArrowCircleRightRounded";
 import Link from "next/link";
 
 export default function Expressions({
@@ -73,43 +77,69 @@ export default function Expressions({
 
   return (
     <div className={styles.main}>
-      <div className={styles.index}>
-        {currentIndex + 1} / {expLength}
+      <div className={styles.upperWrapper}>
+        <div></div>
+        <div className={styles.index}>
+          {currentIndex + 1} / {expLength}
+        </div>
+        <div className={styles.bookmarkButtonWrapper}>
+          <BookmarkBorderRoundedIcon fontSize="large" />
+          <p className={styles.bookmarkButtontext}>Bookmark</p>
+        </div>
       </div>
 
       <div onClick={speakExpression} className={styles.speakButton}>
         <VolumeUpRoundedIcon fontSize="large" />
       </div>
 
-      <div className={styles.upperWrapper}>
-        <div className={styles.expressionWrapper}>
-          <p className={styles.en}>{currentExpression.en}</p>
-          <p className={styles.ko}>{currentExpression.ko}</p>
+      <div className={styles.lowerWrapper}>
+        <div
+          className={`${styles.prevNextButton} ${
+            currentIndex === 0 ? styles.disabled : styles.active
+          }`}
+          onClick={showPrevExpression}
+        >
+          <ArrowCircleLeftRoundedIcon fontSize="large" />
         </div>
 
-        {currentExpression.ex && (
-          <div className={styles.exWrapper}>
-            <p className={styles.exTitle}>💬 Ex</p>
-            <p>{currentExpression.ex}</p>
-            <div className={styles.exLinkWrapper}>
-              <div className={styles.exLinkIcon}>
-                <LaunchRoundedIcon fontSize="medium" />
-              </div>
-              <Link href={exLink} target="_blank">
-                <p className={styles.exLinkText}>다른 예문 확인하기</p>
-              </Link>
-            </div>
+        <div>
+          <div className={styles.expressionWrapper}>
+            <p className={styles.en}>{currentExpression.en}</p>
+            <p className={styles.ko}>{currentExpression.ko}</p>
           </div>
-        )}
+
+          {currentExpression.ex && (
+            <div className={styles.exWrapper}>
+              <p className={styles.exTitle}>💬 Ex</p>
+              <p>{currentExpression.ex}</p>
+              <div className={styles.exLinkWrapper}>
+                <div className={styles.exLinkIcon}>
+                  <LaunchRoundedIcon fontSize="medium" />
+                </div>
+                <Link href={exLink} target="_blank">
+                  <p className={styles.exLinkText}>다른 예문 확인하기</p>
+                </Link>
+              </div>
+            </div>
+          )}
+        </div>
+        <div
+          className={`${styles.prevNextButton} ${
+            currentIndex === expLength - 1 ? styles.disabled : styles.active
+          }`}
+          onClick={showNextExpression}
+        >
+          <ArrowCircleRightRoundedIcon fontSize="large" />
+        </div>
       </div>
-      <div className={styles.buttonWrapper}>
+      {/* <div className={styles.buttonWrapper}>
         <div className={styles.prevNextButton} onClick={showPrevExpression}>
           PREV
         </div>
         <div className={styles.prevNextButton} onClick={showNextExpression}>
           NEXT
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
