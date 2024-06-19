bfd908992648c61d8aa5ff1d706a33b17e97c868
Sara-Jo <sarajo.dev@gmail.com>
2024-06-19 11:40:06 +0900

    add: sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://english-with-suits.vercel.app/",
+  changefreq: "daily",
+  priority: 1,
+  sitemapSize: 7000,
+  generateRobotsTxt: true,
+  exclude: [],
+};
