f278758473575e4ea503264312a44eea5adfb746
Sara-Jo <sarajo.dev@gmail.com>
2024-07-02 14:54:19 +0900

    design: ThemeToggle component

--- src/app/_components/ThemeToggle.tsx
+++ src/app/_components/ThemeToggle.tsx
@@ -1,11 +1,21 @@
 import { useTheme } from "../ThemeContext";
+import styles from "./ThemeToggle.module.css";
+import WbSunnyRoundedIcon from "@mui/icons-material/WbSunnyRounded";
+import DarkModeRoundedIcon from "@mui/icons-material/DarkModeRounded";
 
 const ThemeToggle = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <button onClick={toggleTheme}>
-      {theme === "light" ? "Switch to Dark Mode" : "Switch to Light Mode"}
+    <button className={styles.toggleContainer} onClick={toggleTheme}>
+      <DarkModeRoundedIcon className={styles.icon} fontSize="small" />
+      <WbSunnyRoundedIcon className={styles.icon} fontSize="small" />
+
+      <div
+        className={`${styles.toggleButton} ${
+          theme === "dark" ? styles.dark : ""
+        }`}
+      ></div>
     </button>
   );
 };
