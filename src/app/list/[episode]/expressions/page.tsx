797b0096d804c50c7b75c04dbe13f81554f832e8
Sara-Jo <sarajo.dev@gmail.com>
2024-07-16 10:17:37 +0900

    add: Head for expression page

--- src/app/list/[episode]/expressions/page.tsx
+++ src/app/list/[episode]/expressions/page.tsx
@@ -1,6 +1,7 @@
 import ExpressionClient from "./ExpressionClient";
 import { fetchExpressions } from "@/lib/fetchExpressions";
 import { fetchAllEpisodes } from "@/lib/fetchAllEpisodes";
+import Head from "next/head";
 
 export async function generateStaticParams() {
   const episode = await fetchAllEpisodes();
@@ -16,5 +17,20 @@ export default async function Expressions({
 }) {
   const expressions = await fetchExpressions(params.episode);
 
-  return <ExpressionClient expressions={expressions} />;
+  return (
+    <>
+      <Head>
+        <title>English with Suits: Season 1 Episode {params.episode}</title>
+        <meta
+          name="description"
+          content="Learn business English with English with Suits"
+        />
+        <meta
+          name="keywords"
+          content="English with Suits, Suits, 미드영어, 비즈니스영어, 슈츠, 슈츠영어"
+        />
+      </Head>
+      <ExpressionClient expressions={expressions} />;
+    </>
+  );
 }
