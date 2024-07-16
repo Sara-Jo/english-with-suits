d0f8a36dce5e77f245270a85c48be036a544784e
Sara-Jo <sarajo.dev@gmail.com>
2024-07-16 10:12:04 +0900

    add: Head for list page

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,8 +1,24 @@
 import { fetchAllEpisodes } from "@/lib/fetchAllEpisodes";
 import ListClient from "./listClient";
+import Head from "next/head";
 
 export default async function List() {
   const episodes = await fetchAllEpisodes();
 
-  return <ListClient episodes={episodes} />;
+  return (
+    <>
+      <Head>
+        <title>English with Suits: Season 1</title>
+        <meta
+          name="description"
+          content="Learn business English with English with Suits"
+        />
+        <meta
+          name="keywords"
+          content="English with Suits, Season 1, Suits, 미드영어, 슈츠, 슈츠영어"
+        />
+      </Head>
+      <ListClient episodes={episodes} />
+    </>
+  );
 }
