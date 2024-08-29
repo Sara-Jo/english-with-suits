78c6e3939699496fb095217e95f46a966a294b5f
Sara-Jo <sarajo.dev@gmail.com>
2024-08-29 12:21:00 +0900

    style: delete unnecessary div tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
         <ThemeProvider>
           <AuthProvider>
             <Suspense fallback={<Loading />}>
-              <div>{auth}</div>
+              {auth}
               {children}
             </Suspense>
           </AuthProvider>
