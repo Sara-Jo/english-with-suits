dea65ee414e77f9ea9e56f139e5a499083b2b120
Sara-Jo <sarajo.dev@gmail.com>
2024-08-31 10:19:54 +0900

    fix: change list page into server component

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,6 +1,8 @@
-import { fetchAllEpisodes } from "@/lib/fetchAllEpisodes";
-import ListClient from "./listClient";
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { fetchAllEpisodes } from "@/lib/fetchAllEpisodes";
+import styles from "./page.module.css";
 
 export default async function List() {
   const episodes = await fetchAllEpisodes();
@@ -18,7 +20,37 @@ export default async function List() {
           content="English with Suits, Season 1, Suits, 미드영어, 슈츠, 슈츠영어"
         />
       </Head>
-      <ListClient episodes={episodes} />
+      <div className={styles.container}>
+        <h1 className={styles.title}>Season 1</h1>
+        <div className={styles.list}>
+          {episodes.map((episode) => (
+            <Link
+              key={episode.id}
+              href={`list/${episode.episode}`}
+              className={styles.card}
+            >
+              <div className={styles.imagePlaceholder}>
+                <Image
+                  src={episode.image_url}
+                  alt={episode.title}
+                  priority
+                  fill
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  style={{ objectFit: "cover" }}
+                />
+              </div>
+              <div className={styles.content}>
+                <h2 className={styles.episodeTitle}>
+                  Episode {episode.episode}: {episode.title}
+                </h2>
+                <p className={styles.episodeDescription}>
+                  {episode.description}
+                </p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
