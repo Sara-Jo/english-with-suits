f8b736dc2401f701c97dd441f08d2f1db0846f93
Sara-Jo <sarajo.dev@gmail.com>
2024-09-30 16:19:16 +0900

    refactor: use Array for creating images arrays

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,35 +13,14 @@ import ImageCard from "./_components/ImageCard/ImageCard";
 import useMeasure from "react-use-measure";
 import Link from "next/link";
 
-const images1 = [
-  "/image/home/S1/1.jpg",
-  "/image/home/S1/2.jpg",
-  "/image/home/S1/3.jpg",
-  "/image/home/S1/4.jpg",
-  "/image/home/S1/5.jpg",
-  "/image/home/S1/6.jpg",
-  "/image/home/S1/7.jpg",
-  "/image/home/S1/8.jpg",
-  "/image/home/S1/9.jpg",
-  "/image/home/S1/10.jpg",
-  "/image/home/S1/11.jpg",
-  "/image/home/S1/12.jpg",
-];
-
-const images2 = [
-  "/image/home/S2/1.jpg",
-  "/image/home/S2/2.jpg",
-  "/image/home/S2/3.jpg",
-  "/image/home/S2/4.jpg",
-  "/image/home/S2/5.jpg",
-  "/image/home/S2/6.jpg",
-  "/image/home/S2/7.jpg",
-  "/image/home/S2/8.jpg",
-  "/image/home/S2/9.jpg",
-  "/image/home/S2/10.jpg",
-  "/image/home/S2/11.jpg",
-  "/image/home/S2/12.jpg",
-];
+const images1 = Array.from(
+  { length: 12 },
+  (_, i) => `image/home/S1/${i + 1}.jpg`
+);
+const images2 = Array.from(
+  { length: 12 },
+  (_, i) => `image/home/S2/${i + 1}.jpg`
+);
 
 export default function Home() {
   const router = useRouter();
